d20687d5dddf897cbca9eb9cdc1d2b9d6be004d8
bitcamp <bitcamp@bitcamp.com>
2022-04-14 19:43:55 +0900

    20220414 purchaseController 작업중

---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
@@ -0,0 +1,79 @@
+package com.model2.mvc.web.purchase;
+
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.domain.Purchase;
+import com.model2.mvc.service.product.ProductService;
+import com.model2.mvc.service.purchase.PurchaseService;
+import com.model2.mvc.service.user.UserService;
+
+
+//==> 회원관리 Controller
+@Controller
+public class PurchaseController {
+	
+	///Field
+	@Autowired
+	@Qualifier("purchaseServiceImpl")
+	private PurchaseService purchaseService;
+		
+	public PurchaseController(){
+		System.out.println(this.getClass());
+	}
+	
+	//==> classpath:config/common.properties  ,  classpath:config/commonservice.xml 참조 할것
+	//==> 아래의 두개를 주석을 풀어 의미를 확인 할것
+	@Value("#{commonProperties['pageUnit']}")
+	//@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	//@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+	
+	
+	@RequestMapping("/addPurchaseView.do")
+	public ModelAndView addPurchaseView ( @ModelAttribute("product") Product product) throws Exception {
+
+		System.out.println("/addPurchaseView.do");
+		//Business Logic
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/purchase/addPurchaseView.jsp");
+		
+		return modelAndView;
+	}
+	
+	@RequestMapping("/addPurchase.do")
+	public ModelAndView addPurchase( @ModelAttribute("purchase") Purchase purchase ) throws Exception {
+
+		System.out.println("/addPurchase.do");
+		//Business Logic
+		purchaseService.addPurchase(purchase);
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/purchase/addPurchaseDone.jsp");
+		
+		return modelAndView;
+	}
+
+	
+}
